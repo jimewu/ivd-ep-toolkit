b8a8e9d6f14db4d23e7bac188ee4849805b1a007
jimewu <jimewu@gmail.com>
2021-02-25 11:45:26 +0800

    使用rstudioapi來讀取相對路徑

--- 06-Linearity/ep06-regression.R
+++ 06-Linearity/ep06-regression.R
@@ -1,16 +1,13 @@
 #Part 0. Preparations: configuration area
 #Load package needed
-Packages <- c("dplyr", "ggplot2")
+Packages <- c("dplyr", "ggplot2", "here")
 lapply(Packages, library, character.only = TRUE)
 
-#Set working directory
-DIR <- "ivd-ep-toolkit/06-Linearity"
+#Set working directory using rsudioapi
+DIR <- dirname(rstudioapi::getSourceEditorContext()$path)
 TIME <- format(Sys.time(), "%Y-%m%d-%H%M%S")
 RPRT.DIR <- paste("Report", TIME, sep = "_")
-setwd("~")
-WD <- getwd()
-WD <- paste(WD, DIR, sep = "/")
-setwd(WD)
+setwd(DIR)
 
 #Read setting for analysis
 SET <- read.csv("setting.csv")
